453c5d9f15083114601fb983d90e02d6225b8d63
Joshua Pierce <josh.w.pierce@gmail.com>
2026-01-26 17:35:41 +0100

    added focus on import textfield

--- composeApp/src/commonMain/kotlin/the/autarch/newsgrid/channel/presentation/ImportChannelFromUrlDialog.kt
+++ composeApp/src/commonMain/kotlin/the/autarch/newsgrid/channel/presentation/ImportChannelFromUrlDialog.kt
@@ -1,14 +1,21 @@
 package the.autarch.newsgrid.channel.presentation
 
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PlatformImeOptions
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import the.autarch.newsgrid.LocalSnackbarHostState
@@ -17,11 +24,17 @@ import the.autarch.newsgrid.channel.data.LocalChannelStore
 @Composable
 fun ImportChannelFromUrlDialog(scope: CoroutineScope, onDismiss: () -> Unit) {
 
+    val channelStore = LocalChannelStore.current
+    val snackbarHostState = LocalSnackbarHostState.current
+
     val (importUrl, setImportUrl) = remember { mutableStateOf("") }
     var errorText: String? by remember { mutableStateOf(null) }
 
-    val channelStore = LocalChannelStore.current
-    val snackbarHostState = LocalSnackbarHostState.current
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 
     AlertDialog(
 //            icon = {
@@ -34,10 +47,14 @@ fun ImportChannelFromUrlDialog(scope: CoroutineScope, onDismiss: () -> Unit) {
             TextField(
                 importUrl,
                 setImportUrl,
+                Modifier.focusRequester(focusRequester),
                 supportingText = {
                     errorText?.let { Text(it) }
                 },
-                isError = errorText != null
+                isError = errorText != null,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Uri
+                )
             )
         },
         onDismissRequest = onDismiss,
