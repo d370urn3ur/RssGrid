7ec0aaa55356dc5693a53dba4243b716b1193494
Joshua Pierce <josh.w.pierce@gmail.com>
2026-09-06 16:46:19 +0200

    bumped failed version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 3
-        versionName = "1.0.2"
+        versionName = "1.0.3"
     }
     signingConfigs {
         val keystoreFile = if (keystoreProperties.hasProperty("storeFile")) {
--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -107,7 +107,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "the.autarch.rssgrid"
-            packageVersion = "1.0.2"
+            packageVersion = "1.0.3"
         }
     }
 }
